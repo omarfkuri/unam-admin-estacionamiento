76bd62cfc287a8d2a25c38f3d872040fc98ae85f
Omar <omarfkuri@gmail.com>
2026-05-25 23:41:48 -0600

    Visualizar vehículos y lugares ocupados por usuario

--- js/page/principal/index.js
+++ js/page/principal/index.js
@@ -32,3 +32,50 @@ btn.onclick = async () =>
 
 	location.href = "/unam-admin-estacionamiento/";
 };
+
+const carsResult = await db.getUserCars(auth.currentUser.id);
+
+const carIDs = carsResult.data.map(({id}) => id)
+const spacesResult = await db.getSpaces(carIDs);
+
+const carDisplay = document.getElementById("carDisplay");
+
+carsResult.data.map(car => {
+
+	const carBox = document.createElement("div");
+	
+	const occupied = spacesResult.data
+	.find(({current_car_id}) => car.id === current_car_id);
+
+	carBox.innerHTML = `
+
+	<div class="car card ${ occupied ? "occupied" : "" }">
+		<div class="car-top">
+			<div class="car-title">
+				<span class="car-model">${car.model}</span>
+				<span class="car-color">${car.color}</span>
+			</div>
+			<div class="car-subtitle">
+				<div class="car-brand">${car.brand}</div>
+				<div class="car-plate">${car.plate}</div>
+			</div>
+		</div>
+		<div class="car-bottom">
+			${
+				occupied
+				? `
+					Estacionamiento ${occupied.park_number},
+					Lugar ${occupied.space_number}
+					`
+				: `
+					No estacionado
+					`
+			}
+		</div>
+	</div>
+
+	`;
+
+	carDisplay.append(carBox);
+
+})
