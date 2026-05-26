605add8963a88fa72dd6599ecf8e3c0503942cd9
Omar <omarfkuri@gmail.com>
2026-05-25 23:43:57 -0600

    Mostrar mensaje cuando no hay vahículos registrados

--- js/page/principal/index.js
+++ js/page/principal/index.js
@@ -40,42 +40,47 @@ const spacesResult = await db.getSpaces(carIDs);
 
 const carDisplay = document.getElementById("carDisplay");
 
-carsResult.data.map(car => {
+if (carsResult.data.length == 0)
+{
+	carDisplay.innerText = "No tiene vehículos registrados.";
+}
+else
+	carsResult.data.map(car => {
 
-	const carBox = document.createElement("div");
-	
-	const occupied = spacesResult.data
-	.find(({current_car_id}) => car.id === current_car_id);
+		const carBox = document.createElement("div");
+		
+		const occupied = spacesResult.data
+		.find(({current_car_id}) => car.id === current_car_id);
 
-	carBox.innerHTML = `
+		carBox.innerHTML = `
 
-	<div class="car card ${ occupied ? "occupied" : "" }">
-		<div class="car-top">
-			<div class="car-title">
-				<span class="car-model">${car.model}</span>
-				<span class="car-color">${car.color}</span>
+		<div class="car card ${ occupied ? "occupied" : "" }">
+			<div class="car-top">
+				<div class="car-title">
+					<span class="car-model">${car.model}</span>
+					<span class="car-color">${car.color}</span>
+				</div>
+				<div class="car-subtitle">
+					<div class="car-brand">${car.brand}</div>
+					<div class="car-plate">${car.plate}</div>
+				</div>
 			</div>
-			<div class="car-subtitle">
-				<div class="car-brand">${car.brand}</div>
-				<div class="car-plate">${car.plate}</div>
+			<div class="car-bottom">
+				${
+					occupied
+					? `
+						Estacionamiento ${occupied.park_number},
+						Lugar ${occupied.space_number}
+						`
+					: `
+						No estacionado
+						`
+				}
 			</div>
 		</div>
-		<div class="car-bottom">
-			${
-				occupied
-				? `
-					Estacionamiento ${occupied.park_number},
-					Lugar ${occupied.space_number}
-					`
-				: `
-					No estacionado
-					`
-			}
-		</div>
-	</div>
 
-	`;
+		`;
 
-	carDisplay.append(carBox);
+		carDisplay.append(carBox);
 
-})
+	})
