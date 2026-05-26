6357abcb600c59eae1c83d66c37192229e4645f2
Omar <omarfkuri@gmail.com>
2026-05-25 20:04:48 -0600

    Añadir función "formMaker" para facilitar la creación de formularios.

---
+++ js/lib/formMaker.js
@@ -0,0 +1,56 @@
+/**
+ * Esta función espera que el `form` pasado contenga
+ * un hijo con la clase `.fields`, donde se colocarán
+ * los campos, y un botón que ejecutará la función 
+ * `onSubmit`.
+ * 
+ * @template { string } T
+ * @param { {
+ * 		name: T
+ * 		label: string
+ * 		placeholder: string
+ * 		type: HTMLInputElement["type"]
+ * 		required?: boolean
+ * 		pattern?: RegExp
+ * }[] } fields
+ * @param { HTMLFormElement } form
+ * @param { string } buttonTitle
+ * @param { (inp: { [key in T]: string | null }) => unknown } onSubmit
+ * @returns { void }
+ * */
+export function formMaker(fields, form, onSubmit)
+{
+  const contenedor = document.querySelector(".fields");
+	
+	form.onsubmit = e => 
+	{
+		e.preventDefault();
+
+		const results = Object.fromEntries(
+			(new FormData(form)).entries()
+		);
+
+		onSubmit(results);
+	};
+
+	for (const field of fields)
+	{
+    const label = document.createElement('label');
+    label.textContent = field.label;
+
+    const input = document.createElement('input');
+    input.type  = field.type;
+    input.name  = field.name;
+
+    if (field.pattern != null)
+    	input.pattern = field.pattern;
+
+    if (field.required != null)
+    	input.required = field.required;
+
+    input.classList.add("input-box");
+
+    contenedor.appendChild(label);
+    contenedor.appendChild(input);
+	}
+}
